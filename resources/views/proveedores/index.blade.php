7eb64bcdac47bc8a6a6b6df85f636e9468a3fd22
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-02-20 19:31:30 +0100

    ProveedorController with READ (index) example

---
+++ resources/views/proveedores/index.blade.php
@@ -0,0 +1,8 @@
+@foreach($proveedores as $key => $value)
+  {{ $value->nombre }} <br /><br />
+  {{ $value->direccion }} <br /><br />
+  {{ $value->provincia }} <br /><br />
+  {{ $value->telefono }} <br /><br />
+  {{ $value->nif }} <br /><br />
+  <br /><br />
+@endforeach
