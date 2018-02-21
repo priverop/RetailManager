1860dacd7b603199c4d9c0fd429b12bcf59e1210
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-02-21 22:16:23 +0100

    Material Relationship proveedor

--- app/Material.php
+++ app/Material.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Material extends Model
 {
-    //
+    /**
+    * Obtiene el proveedor
+    */
+    public function proveedor(){
+      return $this->belongsTo('App\Proveedor');
+    }
 }
