5d54a8bc2e9a8122194bd2c4b2c2064eba6db2a2
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-02-22 02:28:44 +0100

    Added Presupuesto id on Partes View

--- resources/views/partes/index.blade.php
+++ resources/views/partes/index.blade.php
@@ -1,5 +1,5 @@
 @foreach($partes as $key => $value)
   Nombre: {{ $value->nombre }} <br /><br />
-  Presupuesto: {{ $value->presupuesto->nombre }} <br /><br />
+  Presupuesto: {{ $value->presupuesto->nombre }} ({{$value->presupuesto->id}}) <br /><br />
   <br /><br />
 @endforeach
