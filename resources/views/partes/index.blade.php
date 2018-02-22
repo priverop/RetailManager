8ffeb3c30dde866335db938ec802fab2c85e7b18
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-02-22 02:41:15 +0100

    Pivot table seeder & Parte View (index) to test it

--- resources/views/partes/index.blade.php
+++ resources/views/partes/index.blade.php
@@ -1,5 +1,9 @@
 @foreach($partes as $key => $value)
   Nombre: {{ $value->nombre }} <br /><br />
   Presupuesto: {{ $value->presupuesto->nombre }} ({{$value->presupuesto->id}}) <br /><br />
-  <br /><br />
+  Materiales:
+  @foreach($value->materiales as $keys =>$values)
+    {{ $values->nombre }} -
+  @endforeach
+  <br /><br /><br /><br />
 @endforeach
