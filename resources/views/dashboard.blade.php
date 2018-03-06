e74f45159d9534a884cb0b93401047bff3a2ffd6
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-03-06 01:12:09 +0100

    Dashboard View

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Modifase - Panel Principal')
+
+@section('content')
+  <h1>Panel Principal</h1>
+  <h3><a href="/presupuestos/10">Presupuesto de Ejemplo</a></h3>
+@endsection
