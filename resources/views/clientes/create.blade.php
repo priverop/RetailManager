9c2cce2c1da9625ac30b8f56266c5342752596ab
MiguelAngelGalazSanchez <32101205+MiguelAngelGalazSanchez@users.noreply.github.com>
2018-03-06 14:08:21 +0100

    intento de create

---
+++ resources/views/clientes/create.blade.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('images/favicon.jpg') }}">
+
+    <!-- CSFR token for ajax call -->
+    <meta name="_token" content="{{ csrf_token() }}"/>
+
+    <title>Añadir cliente</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+    <!-- icheck checkboxes -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/yellow.css">
+
+    <!-- toastr notifications -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
+
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <style>
+        .panel-heading {
+            padding: 0;
+        }
+        .panel-heading ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+        }
+        .panel-heading li {
+            float: left;
+            border-right:1px solid #bbb;
+            display: block;
+            padding: 14px 16px;
+            text-align: center;
+        }
+        .panel-heading li:last-child:hover {
+            background-color: #ccc;
+        }
+        .panel-heading li:last-child {
+            border-right: none;
+        }
+        .panel-heading li a:hover {
+            text-decoration: none;
+        }
+
+        .table.table-bordered tbody td {
+            vertical-align: baseline;
+        }
+        /* icheck checkboxes */
+        .iradio_flat-yellow {
+            background: url(https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/skins/square/yellow.png) no-repeat;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="col-md-8 col-md-offset-2">
+        <h2 class="text-center">Añadir un cliente</h2>
+        <br />
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                
+                    <a href="#" class="add-modal"><li>Introducir cliente</li></a>
+                
+            </div>
+
+            
+        </div><!-- /.panel panel-default -->
+    </div><!-- /.col-md-8 -->
+
+    <!-- Modal form to add a post -->
+    <div id="addModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">×</button>
+                    <h4 class="modal-title"></h4>
+                </div>
+                <div class="modal-body">
+                    <form class="form-horizontal" role="form">
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="title">Nombre:</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="name_add" autofocus>
+                                <small>Min: 2, Max: 32, only text</small>
+                                <p class="errorTitle text-center alert alert-danger hidden"></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="content">Direccion:</label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" id="direccion_add" cols="40" rows="5"></textarea>
+                                <small>Min: 2, Max: 128, only text</small>
+                                <p class="errorContent text-center alert alert-danger hidden"></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="content">Provincia:</label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" id="provincia_add" cols="40" rows="5"></textarea>
+                                <small>Min: 2, Max: 128, only text</small>
+                                <p class="errorContent text-center alert alert-danger hidden"></p>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label col-sm-2" for="content">Telefono:</label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" id="telefono_add" cols="40" rows="5"></textarea>
+                                <small>Min: 2, Max: 128, only text</small>
+                                <p class="errorContent text-center alert alert-danger hidden"></p>
+                            </div>
+                        </div>
+                    </form>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-success add" data-dismiss="modal">
+                            <span id="" class='glyphicon glyphicon-check'></span> Add
+                        </button>
+                        <button type="button" class="btn btn-warning" data-dismiss="modal">
+                            <span class='glyphicon glyphicon-remove'></span> Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap JavaScript -->
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.1/js/bootstrap.min.js"></script>
+
+    <!-- toastr notifications -->
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
+
+    <!-- icheck checkboxes -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/iCheck/1.0.2/icheck.min.js"></script>
+
+    <!-- Delay table load until everything else is loaded -->
+    <script>
+        $(window).load(function(){
+            $('#postTable').removeAttr('style');
+        })
+    </script>
+
+    <script>
+        $(document).ready(function(){
+            $('.published').iCheck({
+                checkboxClass: 'icheckbox_square-yellow',
+                radioClass: 'iradio_square-yellow',
+                increaseArea: '20%'
+            });
+            $('.published').on('ifClicked', function(event){
+                id = $(this).data('id');
+                $.ajax({
+                    type: 'POST',
+                    url: "/clientes",
+                    data: {
+                        '_token': $('input[name=_token]').val(),
+                        'id': id
+                    },
+                    success: function(data) {
+                        location.reload();
+                    },
+                });
+            });
+            $('.published').on('ifToggled', function(event) {
+                $(this).closest('tr').toggleClass('warning');
+            });
+        });
+
+    </script>
+
+    <!-- AJAX CRUD operations -->
+    <script type="text/javascript">
+        
+        var form_action = $("#addParteForm").attr("action");
+        
+        // add a new post
+        $(document).on('click', '.add-modal', function() {
+            $('.modal-title').text('Add');
+            $('#addModal').modal('show');
+        });
+        $('.modal-footer').on('click', '.add', function() {
+            $.ajax({
+                type: 'POST',
+                url: "/clientes",
+                data: {
+                    '_token': $('input[name=_token]').val(),
+                    'nombre': $('#nombre_add').val(),
+                    'telefono': $('#telefono_add').val(),
+                    'direccion': $('#direccion_add').val(),
+                    'provincia': $('#provincia_add').val()
+                    
+                },
+                success: function(data) {
+                    $('.errorTitle').addClass('hidden');
+                    $('.errorContent').addClass('hidden');
+
+                    if ((data.errors)) {
+                        setTimeout(function () {
+                            $('#addModal').modal('show');
+                            toastr.error('Validation error!', 'Error Alert', {timeOut: 5000});
+                        }, 500);
+
+                        
+                    } else {
+                        toastr.success('Successfully added Post!', 'Success Alert', {timeOut: 5000});
+                        $('#postTable').append("<tr class='item" + data.id + "'><td>" + data.id + "</td><td>" + data.title + "</td><td>" + data.content + "</td><td class='text-center'><input type='checkbox' class='new_published' data-id='" + data.id + " '></td><td>Right now</td><td><button class='show-modal btn btn-success' data-id='" + data.id + "' data-title='" + data.title + "' data-content='" + data.content + "'><span class='glyphicon glyphicon-eye-open'></span> Show</button> <button class='edit-modal btn btn-info' data-id='" + data.id + "' data-title='" + data.title + "' data-content='" + data.content + "'><span class='glyphicon glyphicon-edit'></span> Edit</button> <button class='delete-modal btn btn-danger' data-id='" + data.id + "' data-title='" + data.title + "' data-content='" + data.content + "'><span class='glyphicon glyphicon-trash'></span> Delete</button></td></tr>");
+                        $('.new_published').iCheck({
+                            checkboxClass: 'icheckbox_square-yellow',
+                            radioClass: 'iradio_square-yellow',
+                            increaseArea: '20%'
+                        });
+                        $('.new_published').on('ifToggled', function(event){
+                            $(this).closest('tr').toggleClass('warning');
+                        });
+                        $('.new_published').on('ifChanged', function(event){
+                            id = $(this).data('id');
+                            $.ajax({
+                                type: 'POST',
+                                url: "/clientes",
+                                data: {
+                                    '_token': $('input[name=_token]').val(),
+                                    'id': id
+                                },
+                                success: function(data) {
+                                    location.reload();
+                                },
+                            });
+                        });
+                    }
+                },
+            });
+        });
+    </script>
