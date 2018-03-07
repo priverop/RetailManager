fe181b7c3678ff3980b31b25b6cffb107198d25d
MiguelAngelGalazSanchez <32101205+MiguelAngelGalazSanchez@users.noreply.github.com>
2018-03-07 12:44:05 +0100

    Add files via upload

--- resources/views/presupuestos/index.blade.php
+++ resources/views/presupuestos/index.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
 
-@section('title', 'Presupuestos')
+@section('title', 'Proveedores')
 
 @section('content')
-
-  @foreach($presupuestos as $key => $value)
-    Nombre: {{ $value->nombre }} <br /><br />
-    Obra para el Cliente: {{ $value->obra->cliente->nombre }} <br /><br />
-    <br /><br />
+  @foreach($proveedores as $key => $value)
+    {{ $value->nombre }} <br /><br />
+    {{ $value->direccion }} <br /><br />
+    {{ $value->provincia }} <br /><br />
+    {{ $value->telefono }} <br /><br />
+    {{ $value->nif }} <br /><br />
+   
+<input type="button" value="Mostrar Proveedores" onClick="window.location = 'proveedores/{{ $value->id }}'"> 
+<hr />
   @endforeach
 @endsection
