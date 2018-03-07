c3b056a77534b330db52ae3e181059df2f929776
MiguelAngelGalazSanchez <32101205+MiguelAngelGalazSanchez@users.noreply.github.com>
2018-03-07 13:52:55 +0100

    cambios con tablas bonitas

--- resources/views/proveedores/show.blade.php
+++ resources/views/proveedores/show.blade.php
@@ -8,11 +8,12 @@
   <h1>Proveedor Determinado</h1>
   <div class="row mt-5 p-3 border">
     <div class="col-xs-12">
-    <p>{{ $proveedor->nombre }} </p>
-   <p> {{ $proveedor->direccion }}</p>
-   <p> {{ $proveedor->provincia }}</p>
-   <p> {{ $proveedor->telefono }} </p>
-   <p> {{ $proveedor->nif }}</p>
+    <p>Nombre: {{ $proveedor->nombre }} </p>
+    <p>Dirección: {{ $proveedor->direccion }}</p>
+    <p>Provincia:{{ $proveedor->provincia }}</p>
+    <p>Teléfono: {{ $proveedor->telefono }} </p>
+    <p>Nif: {{ $proveedor->nif }}</p>
+    <p>Id: {{ $proveedor->id }}</p>
       
     </div>
   </div>
