035afee2a328dd2521c09185fcaf95d647fc0f17
DESKTOP-OLJ5GKM\Carns <carnsart@gmail.com>
2018-03-12 17:49:35 +0100

    Mostrar datos presupuesto index.blade

--- resources/views/presupuestos/index.blade.php
+++ resources/views/presupuestos/index.blade.php
@@ -10,37 +10,50 @@
 
 
   <h2>Lista de Presupuestos</h2>
-      
-      
+
+
   <table class="table table-striped">
     <thead>
       <tr>
-        <th>Nombre</th>
-        <th>Obra para el cliente</th>
+        <th>Concepto</th>
+        <th>Unidades</th>
+        <th>Obra</th>
+        <th>Cliente</th>
+        <th>Fecha</th>
+        <th>Estado</th>
+        <th>Características</th>
+        <th>Beneficio</th>
+        <th>Precio Final</th>
         <th>ID Presupuesto</th>
-        
-        
+
+
+
       </tr>
     </thead>
     <tbody>
-       
+
           @foreach($presupuestos as $key => $value)
-        <tr>
-        <td> <a href='presupuestos/{{ $value->id }}'> {{ $value->nombre }}</a> </td>
-        
-        
-        
-        <td>{{ $value->obra->cliente->nombre }}</td>
-            
-        <td>{{ $value->id}}</td>
-          
-        </tr>
-          
-          
+              <tr>
+
+                <td> <a href='presupuestos/{{ $value->id }}'> {{ $value->concepto }}</a> </td>
+                <td> {{ $value->unidades }} </td>
+                <td>{{ $value->obra->id}}</td>
+                <td>{{ $value->obra->cliente->nombre }}</td>
+                <td> {{ $value->fecha }} </td>
+                <td> {{ $value->estado }} </td>
+                <td> {{ $value->caracteristicas }} </td>
+                <td> {{ $value->beneficio }} </td>
+                <td> {{ $value->precio_final }} </td>
+
+
+                <td>{{ $value->id}}</td>
+              </tr>
+
+
           @endforeach
-          
-      
-      
+
+
+
     </tbody>
   </table>
 </div>
