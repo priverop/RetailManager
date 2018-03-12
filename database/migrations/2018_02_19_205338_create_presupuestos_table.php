17f8761ff313119cab9b46f4721d7530cd37296c
DESKTOP-OLJ5GKM\Carns <carnsart@gmail.com>
2018-03-12 17:48:36 +0100

    Atributos completos tabla presupuestos

--- database/migrations/2018_02_19_205338_create_presupuestos_table.php
+++ database/migrations/2018_02_19_205338_create_presupuestos_table.php
@@ -15,9 +15,14 @@ class CreatePresupuestosTable extends Migration
     {
         Schema::create('presupuestos', function (Blueprint $table) {
             $table->increments('id')->unsigned()->unique();
-            $table->string('nombre');
             $table->integer('obra_id')->unsigned();
             $table->foreign('obra_id')->references('id')->on('obras')->onDelete('cascade');
+            $table->timestamp('fecha');
+            $table->string('concepto')->default("Mueble");
+            $table->string('caracteristicas')->nullable();
+            $table->integer('unidades')->default(1);
+            $table->string('estado')->default('por comprobar');
+            $table->integer('beneficio')->default(0.3);
             $table->integer('precio_final')->default(0);
             $table->timestamps();
         });
