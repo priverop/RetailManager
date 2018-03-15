4ec389cc61e3408e3aa679fcddf3c16c1a4d0d77
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-03-15 14:21:24 +0100

    Moved RefreshTotalPrize and UpdatePrize to materialController (see Trello)

--- app/Http/Controllers/PresupuestoController.php
+++ app/Http/Controllers/PresupuestoController.php
@@ -10,31 +10,6 @@ use Illuminate\Support\Facades\DB;
 class PresupuestoController extends Controller
 {
 
-  /**
-  * Recalcula el precio final del presupuesto y lo actualiza en la ddbb
-  *
-  * @return Integer precio final
-  */
-  public function refreshTotalPrize(int $presupuesto_id){
-
-
-    $prizes = DB::select(DB::raw("SELECT partes.id, material_parte.*, material_proveedor.precio
-    FROM material_proveedor, material_parte, partes
-    WHERE material_proveedor.material_id = material_parte.material_id
-    AND partes.presupuesto_id = 1
-    AND material_parte.parte_id = partes.id
-    AND material_proveedor.proveedor_id = material_parte.proveedor_id
-    AND material_proveedor.material_id = material_parte.material_id"));
-
-    $precioTotal = 0;
-
-    foreach($prizes as $key => $value){
-      $precioTotal += $value->precio;
-    }
-
-    return $this->updatePrize($precioTotal, $presupuesto_id);
-  }
-
     /**
      * Display a listing of the resource.
      *
@@ -110,17 +85,6 @@ class PresupuestoController extends Controller
       // $presupuesto = Presupuesto::find($id);
 
       $presupuesto = Presupuesto::find($id)->update($request->all());
-
-      return response()->json($presupuesto);
-    }
-
-    public function updatePrize(int $precio, $presupuesto_id)
-    {
-      $presupuesto = Presupuesto::find($presupuesto_id);
-
-      $presupuesto->precio_final = $precio;
-
-      $presupuesto->save();
 
       return response()->json($presupuesto);
     }
