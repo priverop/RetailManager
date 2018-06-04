f6d0ff7f8247a8ea631b4ef645b5f5ffa7313133
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-06-04 20:15:00 +0200

    Materiales.delete fixed

--- resources/views/materiales/index.blade.php
+++ resources/views/materiales/index.blade.php
@@ -24,14 +24,15 @@
     <tbody>
       @foreach($pivotTable as $key => $value)
       <tr>
-        <td>{{ $key }}</td>
+        <td>{{ $value->material_id }}</td>
         <td><a href='materiales/{{ $value->material_id }}'> {{ $value->m_nombre}}</a> </td>
         <td>{{ $value->m_tipo}}</td>
         <td><a href='proveedores/{{ $value->proveedor_id }}'>{{ $value->p_nombre }}</a> </td>
         <td>{{ $value->precio }}</td>
         <td>
           <button type="button" class="btn btn-outline-primary btn-sm mb-1" onclick="editarMaterial( {{$value->material_id}}, {{ $value->proveedor_id }})">Editar</button>
-          <button type="button" class="btn btn-outline-primary btn-sm" onclick="eliminarMaterial({{ $key }})">Borrar</button>
+          <button type="button" class="btn btn-outline-primary btn-sm" onclick="eliminarMaterial(this)">Borrar</button>
+          <button type="hidden" value="{{route('materiales.destroy', ['id' => $value->material_id])}}">
         </td>
       </tr>
       @endforeach
@@ -41,6 +42,12 @@
 
 <script type="text/javascript">
 $(function() {
+  $.ajaxSetup({
+    headers: {
+      'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+  });
+
   $("#indexMaterial").DataTable({
     "language": {
           "url": "{{ asset('/js/datatable_spanish.json') }}"
@@ -81,6 +88,33 @@ function editarMaterial(material_id, proveedor_id){
     $('#addModal').modal('show');
   });
 }
+
+function editarMaterial(material_id, proveedor_id){
+  var form_action = "{{route('materiales.create')}}";
+
+  $.ajax({
+    dataType: 'json',
+    type: 'GET',
+    url: form_action,
+    data: {material_id: material_id, proveedor_id: proveedor_id }
+  }).done(function(data){
+    $('#addMaterial').parent().prepend(data);
+    $('#addModal').modal('show');
+  });
+}
+
+function eliminarMaterial(elemento){
+  var form_action = $(elemento).next().val();
+
+  $.ajax({
+    dataType: 'json',
+    type: 'DELETE',
+    url: form_action
+  }).done(function(data){
+    location.reload();
+  });
+}
+
 </script>
 
 @endsection
