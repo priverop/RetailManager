cde1e1522fc1e7eff996799bb9a4160034d48f0c
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-06-08 20:29:09 +0200

    Changed Factory Presupuesto to generate Name

--- database/factories/PresupuestosFactory.php
+++ database/factories/PresupuestosFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Presupuesto::class, function (Faker $faker) {
     return [
-        // 'nombre' => $faker->firstName(),
+        'concepto' => $faker->firstName(),
         'obra_id' => $faker->numberBetween($min = 1, $max = 10),
     ];
 });
