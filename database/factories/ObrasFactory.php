fdb4d85b12071dcf676a4419f7abaab52f7d280f
DESKTOP-OLJ5GKM\Carns <carnsart@gmail.com>
2018-07-02 21:44:23 +0200

    Dummy data para versiones de obras

--- database/factories/ObrasFactory.php
+++ database/factories/ObrasFactory.php
@@ -3,8 +3,10 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Obra::class, function (Faker $faker) {
+    static $number = 1;
     return [
         'nombre'      => $faker->firstName,
+        'v_id'        => $number++,
         'cliente_id'  => $faker->numberBetween($min = 1, $max = 10),
         'fecha'       => $faker->date($format = 'Y-m-d', $max = 'now'),
     ];
